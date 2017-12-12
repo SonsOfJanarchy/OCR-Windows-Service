0f4f953fd4232d4c56bdfbc7c1403bf974f8bd95
PvSpencerHochmann <shochmann@practicevelocity.com>
2017-12-12 15:11:24 -0600

    another commit

---
+++ OCR_Console/OCR_Console/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Net.Mime;
+using Newtonsoft.Json;
+using tessnet2;
+
+namespace OCR_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateJson();
+            CleanUpFiles();
+        }
+
+        private static void CreateJson()
+        {
+            var files = new DirectoryInfo(@"C:\WindowsServiceInput\").GetFiles();
+            foreach (var file in files)
+            {
+                if (!File.Exists(@"C:\WindowsServiceOutput\" + file.Name))
+                {
+                    var ocr = new Tesseract();
+                    var image = Image.FromFile(file.FullName);
+                    ocr.Init(@"..\..\Content\tessdata", "eng", false);
+                    var result = ocr.DoOCR((Bitmap)image, Rectangle.Empty);
+                    List<string> data = new List<string>();
+                    foreach (Word word in result)
+                        data.Add(word.Text);
+
+                    string json = JsonConvert.SerializeObject(data.ToArray());
+                    System.IO.File.WriteAllText(
+                        @"C:\WindowsServiceOutput\" + Path.GetFileNameWithoutExtension(file.Name) + ".json", json);
+                }
+            }
+        }
+
+        private static void CleanUpFiles()
+        {
+            var files = new DirectoryInfo(@"C:\WindowsServiceInput\");
+
+            foreach (FileInfo file in files.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
