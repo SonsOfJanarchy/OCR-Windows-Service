420933a115ac57f08c367efe892e71a96ecd6374
shochmann <shochmann@practicevelocity.com>
2017-12-20 16:56:57 -0600

    impossibur

--- OCR_Console/OCR_Console/Program.cs
+++ OCR_Console/OCR_Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
@@ -15,6 +16,7 @@ namespace OCR_Console
     class Program
     {
         private static System.IO.FileSystemWatcher m_BarCodeWatcher = new System.IO.FileSystemWatcher(@"C:\BarCodeServiceInput\");
+        private static System.IO.FileSystemWatcher m_OcrWatcher = new System.IO.FileSystemWatcher(@"C:\OcrServiceInput\");
         static ManualResetEvent _quitEvent = new ManualResetEvent(false);
 
         static void Main(string[] args)
@@ -24,95 +26,109 @@ namespace OCR_Console
                 eArgs.Cancel = true;
             };
             //CreateJson();
-            
-            m_BarCodeWatcher.EnableRaisingEvents = true;
-            m_BarCodeWatcher.Created += (o, eventArgs) => ReadBarCode(o, eventArgs);
+
+            m_OcrWatcher.EnableRaisingEvents = true;
+            m_OcrWatcher.Created += (o, eventArgs) => CreateJson(o, eventArgs);
 
             _quitEvent.WaitOne();
         }
 
         private static void ReadBarCode(Object sender, FileSystemEventArgs e)
         {
-            System.Threading.Thread.Sleep(2000);
-            var reader = new BarcodeReader();
-            string dbrLicenseKeys = "t0068MgAAAE/4ptMgtvYh/3/aHYqyZSg9d1L+JaBCahbvfx+h4pmOrJUH1XLf+UG9o+yhRBil3A3DocqUs7B7uUkWBm8BdD0=";
-            reader.LicenseKeys = dbrLicenseKeys;
-            ReaderOptions option = new ReaderOptions();
-            option.BarcodeFormats = BarcodeFormat.PDF417;
-            reader.ReaderOptions = option;
-            var files = new DirectoryInfo(@"C:\BarCodeServiceInput\").GetFiles();
-
-            foreach (var file in files)
+            try
             {
-                if (!File.Exists(@"C:\WindowsServiceOutput\" + file.Name))
+                Console.WriteLine("New files detected in directory - getting ready for processing");
+                System.Threading.Thread.Sleep(2000);
+                var reader = new BarcodeReader();
+                string dbrLicenseKeys = "t0068MgAAAE/4ptMgtvYh/3/aHYqyZSg9d1L+JaBCahbvfx+h4pmOrJUH1XLf+UG9o+yhRBil3A3DocqUs7B7uUkWBm8BdD0=";
+                reader.LicenseKeys = dbrLicenseKeys;
+                ReaderOptions option = new ReaderOptions();
+                option.BarcodeFormats = BarcodeFormat.PDF417;
+                reader.ReaderOptions = option;
+                var files = new DirectoryInfo(@"C:\BarCodeServiceInput\").GetFiles();
+                Console.WriteLine("Getting all available files in directory");
+
+                foreach (var file in files)
                 {
-                    BarcodeResult[] result = reader.DecodeFile(file.FullName);
-                    if (result == null)
+                    if (!File.Exists(@"C:\BarcodeServiceOutput\" + file.Name) && (file.Extension == ".jpg" || file.Extension == ".bmp"))
                     {
-                        file.Delete();
-                        return;
+                        Console.WriteLine("Beginning barcode processing for file - " + file.Name);
+                        BarcodeResult[] result = reader.DecodeFile(file.FullName);
+                        if (result == null)
+                        {
+                            file.Delete();
+                            return;
+                        }
+                        var data = result.FirstOrDefault(x => x.BarcodeFormat == BarcodeFormat.PDF417).BarcodeText;
+                        IdentificationReturnModel model = new IdentificationReturnModel();
+
+                        var strings = data.Split(new string[] {"\n"}, StringSplitOptions.None);
+                        foreach (var str in strings)
+                        {
+                            if (str.StartsWith("DBB"))
+                            {
+                                var bday = str.Replace("DBB", "");
+                                bday = bday.Insert(4, "/");
+                                bday = bday.Insert(7, "/");
+                                model.dateofBirth = Convert.ToDateTime(bday);
+                            }
+                            else if (str.Contains("DAA"))
+                            {
+                                var split = str.Split(',');
+                                if (split.Any())
+                                {
+                                    var beginIndex = split[0].IndexOf("DAA", StringComparison.Ordinal) + 3;
+                                    model.lastName = split[0].Substring(beginIndex, (split[0].Length - beginIndex));
+                                }
+
+                                if (split.Count() > 1)
+                                {
+                                    model.firstName = split[1];
+                                }
+                                if (split.Count() > 2)
+                                {
+                                    model.middleName = split[2];
+                                }
+                            }
+                            else if (str.StartsWith("DAG"))
+                            {
+                                model.address1 = str.Replace("DAG", "");
+                            }
+                            else if (str.StartsWith("DAI"))
+                            {
+                                model.city = str.Replace("DAI", "");
+                            }
+                            else if (str.StartsWith("DAJ"))
+                            {
+                                model.state = str.Replace("DAJ", "");
+                            }
+                            else if (str.StartsWith("DAK"))
+                            {
+                                var zip = str.Replace("DAK", "");
+                                zip = zip.TrimEnd();
+                                zip = zip.Insert(5, "-");
+                                model.zip = zip;
+                            }
+                            else if (str.StartsWith("DBC"))
+                            {
+                                model.sex = str.Replace("DBC", "");
+                            }
+                        }
+
+                        var json = JsonConvert.SerializeObject(model);
+                        System.IO.File.WriteAllText(@"C:\BarcodeServiceOutput\" + Path.GetFileNameWithoutExtension(file.Name) + ".json", json);
                     }
-                    var data = result.FirstOrDefault(x => x.BarcodeFormat == BarcodeFormat.PDF417).BarcodeText;
-                    IdentificationReturnModel model = new IdentificationReturnModel();
-
-                    var strings = data.Split(new string[] {"\n"}, StringSplitOptions.None);
-                    foreach (var str in strings)
-                    {
-                        if (str.StartsWith("DBB"))
-                        {
-                            var bday = str.Replace("DBB", "");
-                            bday = bday.Insert(4, "/");
-                            bday = bday.Insert(7, "/");
-                            model.dateofBirth = Convert.ToDateTime(bday);
-                        }
-                        else if (str.Contains("DAA"))
-                        {
-                            var split = str.Split(',');
-                            if (split.Any())
-                            {
-                                var beginIndex = split[0].IndexOf("DAA", StringComparison.Ordinal) + 3;
-                                model.lastName = split[0].Substring(beginIndex, (split[0].Length - beginIndex));
-                            }
-
-                            if (split.Count() > 1)
-                            {
-                                model.firstName = split[1];
-                            }
-                            if (split.Count() > 2)
-                            {
-                                model.middleName = split[2];
-                            }
-                        }
-                        else if (str.StartsWith("DAG"))
-                        {
-                            model.address1 = str.Replace("DAG", "");
-                        }
-                        else if (str.StartsWith("DAI"))
-                        {
-                            model.city = str.Replace("DAI", "");
-                        }
-                        else if (str.StartsWith("DAJ"))
-                        {
-                            model.state = str.Replace("DAJ", "");
-                        }
-                        else if (str.StartsWith("DAK"))
-                        {
-                            var zip = str.Replace("DAK", "");
-                            zip = zip.TrimEnd();
-                            zip = zip.Insert(5, "-");
-                            model.zip = zip;
-                        }
-                        else if (str.StartsWith("DBC"))
-                        {
-                            model.sex = str.Replace("DBC", "");
-                        }
-                    }
-
-                    var json = JsonConvert.SerializeObject(model);
-                    System.IO.File.WriteAllText(
-                        @"C:\WindowsServiceOutput\" + Path.GetFileNameWithoutExtension(file.Name) + ".json", json);
+                    file.Delete();
+                    Console.WriteLine("Finished processing file - " + file.Name);
                 }
-                file.Delete();
+                Console.WriteLine("All processing complete");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                Console.WriteLine(exception.InnerException);
+                Console.WriteLine(exception.Message);
+                throw;
             }
         }
 
@@ -121,48 +137,68 @@ namespace OCR_Console
             
         }
 
-        private static void CreateJson()
+        private static void CreateJson(Object sender, FileSystemEventArgs e)
         {
-            var dataItems = new OCRRawDataModel();
-            var files = new DirectoryInfo(@"C:\OcrServiceInput\").GetFiles();
-            foreach (var file in files)
+            try
             {
-                if (!File.Exists(@"C:\WindowsServiceOutput\" + file.Name))
+                Console.WriteLine("New files detected in directory - getting ready for processing");
+                System.Threading.Thread.Sleep(2000);
+                var dataItems = new OCRRawDataModel();
+                var files = new DirectoryInfo(@"C:\OcrServiceInput\").GetFiles();
+                Console.WriteLine("Getting all available files in directory");
+                foreach (var file in files)
                 {
-                    var ocr = new Tesseract();
-                    var bitMapImage = (Bitmap) Image.FromFile(file.FullName);
-                    var resizedImage = Resize(bitMapImage, (3000), (3000), false);
-                    var bit = (Bitmap)resizedImage;
-                    bit.SetResolution(300,300);
-
-                    var blackAndWhiteImage = BlackAndWhite(bit, new Rectangle(0,0, resizedImage.Width, resizedImage.Height));
-                    //getanotherimage.SetResolution(1000, 1000);
-                    blackAndWhiteImage.Save(@"C:\WindowsServiceOutput\BlackAndWhite.bmp");
-                    ocr.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.,/");
-                    ocr.Init(@"..\..\Content\tessdata", "eng", false);
-                    var result = ocr.DoOCR(blackAndWhiteImage, Rectangle.Empty);
-                    dataItems.DataList = new List<OCRRawDataModel.RawDataItem>();
-
-                    foreach (Word word in result)
+                    if (!File.Exists(@"C:\OCR\OCRServiceOutput\" + file.Name) && (file.Extension == ".jpg" || file.Extension == ".bmp"))
                     {
-                        var item = new OCRRawDataModel.RawDataItem();
-                        item.Value = word.Text;
-                        item.Confidence = (int)word.Confidence;
-                        item.LineIndex = word.LineIndex;
-                        dataItems.DataList.Add(item);
-                    }
-                    var anotherjson = JsonConvert.SerializeObject(dataItems);
-                    
-                    var mapper = new IdentificationCardMapper();
-                    var mappedObjects = mapper.MapDriversLicenseData(dataItems);
-                    var json = JsonConvert.SerializeObject(dataItems);
-                    System.IO.File.WriteAllText(@"C:\WindowsServiceOutput\" + Path.GetFileNameWithoutExtension(file.Name) + ".json", json);
+                        Console.WriteLine("Beginning OCR processing for file - " + file.Name);
+                        var ocr = new Tesseract();
+                        var bitMapImage = (Bitmap) Image.FromFile(file.FullName);
+                        var resizedImage = Resize(bitMapImage, (3000), (3000), false);
+                        bitMapImage.Dispose();
+                        var bit = (Bitmap)resizedImage;
+                        bit.SetResolution(300,300);
 
-                    bitMapImage.Dispose();
+                        Console.WriteLine("Beginning conversion to black and white");
+                        var blackAndWhiteImage = BlackAndWhite(bit, new Rectangle(0, 0, resizedImage.Width, resizedImage.Height));
+                        Console.WriteLine("Finished converting image to black and white");
+
+                        //blackAndWhiteImage.Save(@"C:\WindowsServiceOutput\BlackAndWhite.bmp");
+                        ocr.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.,/");
+                        ocr.Init(@"..\..\Content\tessdata", "eng", false);
+                        Console.WriteLine("Beginning OCR");
+                        var result = ocr.DoOCR(blackAndWhiteImage, Rectangle.Empty);
+                        Console.WriteLine("Finished OCR");
+                        dataItems.DataList = new List<OCRRawDataModel.RawDataItem>();
+
+                        foreach (Word word in result)
+                        {
+                            var item = new OCRRawDataModel.RawDataItem();
+                            item.Value = word.Text;
+                            item.Confidence = (int)word.Confidence;
+                            item.LineIndex = word.LineIndex;
+                            dataItems.DataList.Add(item);
+                        }
+
+                        var mapper = new IdentificationCardMapper();
+                        var mappedObjects = mapper.MapDriversLicenseData(dataItems);
+                        Console.WriteLine("Finished mapping data");
+                        var json = JsonConvert.SerializeObject(mappedObjects);
+                        System.IO.File.WriteAllText(@"C:\OCRServiceOutput\" + Path.GetFileNameWithoutExtension(file.Name) + ".json", json);
+
+                        bitMapImage.Dispose();
+                        Console.WriteLine("Finished processing file - " + file.Name);
+                    }
+                    file.Delete();
                 }
-                file.Delete();
+                Console.WriteLine("All processing complete");
             }
-            
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                Console.WriteLine(exception.InnerException);
+                Console.WriteLine(exception.Message);
+                throw;
+            }
         }
 
         private int PerceivedBrightness(Color c)
@@ -175,36 +211,42 @@ namespace OCR_Console
 
         private static Bitmap BlackAndWhite(Bitmap image, Rectangle rectangle)
         {
-            Bitmap blackAndWhite = new System.Drawing.Bitmap(image.Width, image.Height);
-
-            // make an exact copy of the bitmap provided
-            using (Graphics graphics = System.Drawing.Graphics.FromImage(blackAndWhite))
-                graphics.DrawImage(image, new System.Drawing.Rectangle(0, 0, image.Width, image.Height),
-                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
-
-            // for every pixel in the rectangle region
-            for (Int32 xx = rectangle.X; xx < rectangle.X + rectangle.Width && xx < image.Width; xx++)
+            try
             {
-                for (Int32 yy = rectangle.Y; yy < rectangle.Y + rectangle.Height && yy < image.Height; yy++)
+                Bitmap blackAndWhite = new System.Drawing.Bitmap(image.Width, image.Height);
+                // make an exact copy of the bitmap provided
+                using (Graphics graphics = System.Drawing.Graphics.FromImage(blackAndWhite))
+                    graphics.DrawImage(image, new System.Drawing.Rectangle(0, 0, image.Width, image.Height),
+                        new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+                // for every pixel in the rectangle region
+                for (Int32 xx = rectangle.X; xx < rectangle.X + rectangle.Width && xx < image.Width; xx++)
                 {
-                    // average the red, green and blue of the pixel to get a gray value
-                    Color pixel = blackAndWhite.GetPixel(xx, yy);
-                    Int32 sum = (pixel.R + pixel.G + pixel.B);
-
-                    if (sum <= 400)
+                    for (Int32 yy = rectangle.Y; yy < rectangle.Y + rectangle.Height && yy < image.Height; yy++)
                     {
-                        blackAndWhite.SetPixel(xx, yy, Color.FromArgb(0, 0, 0));
+                        // average the red, green and blue of the pixel to get a gray value
+                        Color pixel = blackAndWhite.GetPixel(xx, yy);
+                        Int32 sum = (pixel.R + pixel.G + pixel.B);
+
+                        if (sum <= 400)
+                        {
+                            blackAndWhite.SetPixel(xx, yy, Color.FromArgb(0, 0, 0));
+                        }
+                        else
+                        {
+                            blackAndWhite.SetPixel(xx, yy, Color.FromArgb(255, 255, 255));
+                        }   
                     }
-                    else
-                    {
-                        blackAndWhite.SetPixel(xx, yy, Color.FromArgb(255, 255, 255));
-                    }       
-                            
-                    
                 }
+                return blackAndWhite;
             }
-
-            return blackAndWhite;
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
 
         private static Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
